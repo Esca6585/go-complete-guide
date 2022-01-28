8d59ec59a283dd800bd2539806a3d38d09bfea90
WORKSTATION-01\User_pc <babamyratnurgeldiyev@gmail.com>
2022-01-28 12:27:55 +0500

    Golang

---
+++ exercise-1/exercise.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var firstName string = "Babamyrat"
+	// var lastName string = "Nurgeldiyev"
+
+	// firstName = "Babamyrat"
+	// lastName = "Nurgeldiyev"
+
+	firstName := "Babamyrat"
+	lastName := "Nurgeldiyev"
+
+	currentYear := 2022
+	birthYear := 1998
+	age := currentYear - birthYear
+	nextYear := currentYear + 1
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(currentYear)
+	fmt.Println(birthYear)
+	fmt.Println(age)
+	fmt.Println(nextYear)
+
+}
