95269a6c543a3b5c6a2674994e2e909727908283
WORKSTATION-01\User_pc <babamyratnurgeldiyev@gmail.com>
2022-01-29 12:00:45 +0500

    deleted bmi

--- bmi-calculator/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/babamyrat1003/go-complete-guide
-
-go 1.17
--- bmi-calculator/info/info.go
+++
@@ -1,6 +0,0 @@
-package info
-
-const mainTitle = "BMI Calculator"
-const seperator = "------------------"
-const weightPromt = "Please enter your weight (kg): "
-const heightPromt = "Please enter your height (m): "
